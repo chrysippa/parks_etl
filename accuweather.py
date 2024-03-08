24e94fb5bb2068a7090d81912d20049e3f03f661
Cassandra Blair <65232461+chrysippa@users.noreply.github.com>
2024-03-08 17:44:45 -0600

    Add accuweather pseudocode

---
+++ accuweather.py
@@ -0,0 +1,12 @@
+# get park ids and accuweather location key codes
+
+# api_key = 
+
+# url = f"http://dataservice.accuweather.com/forecasts/v1/daily/5day/{}?apikey={}&details=true"
+
+# try:
+#   response = 
+# except HTTP response 500:
+#   try again
+# except HTTP response not 400 or 500:
+#   stuff
