3ab117ba2e53b11efa8584a2b53d01728d8c0243
Cassandra Blair <65232461+chrysippa@users.noreply.github.com>
2024-03-13 13:29:02 -0500

    Pull Visual Crossing data

---
+++ visual_crossing.py
@@ -0,0 +1,46 @@
+import requests
+import pandas
+from visual_crossing_key import visual_crossing_api_key
+
+# get park ids and city, state
+
+# create 2 dataframes: 1 today, 1 tomorrow
+
+# Test case - Afton State Park
+city = "Hastings, MN"
+
+url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}/last1days/next1days?unitGroup=us&elements=datetime%2Cname%2Ctempmax%2Cfeelslikemax%2Chumidity%2Cprecip%2Cprecipprob%2Cpreciptype%2Csnowdepth%2Cwindspeed%2Ccloudcover%2Cuvindex%2Csunrise%2Csunset%2Cdescription%2Cicon&include=days%2Calerts%2Cfcst%2Cstatsfcst%2Cobs%2Cremote%2Cstats&key={visual_crossing_api_key}&contentType=json"
+
+# Handle these status codes: 
+# 400 BAD_REQUEST The format of the API is incorrect or an invalid parameter or combination of parameters was supplied
+# 401 UNAUTHORIZED There is a problem with the API key, account or subscription. May also be returned if a feature is requested for which the account does not have access to.
+# 404 NOT_FOUND The request cannot be matched to any valid API request endpoint structure.
+# 429 TOO_MANY_REQUESTS The account has exceeded their assigned limits. 
+# 500 INTERNAL_SERVER_ERROR A general error has occurred processing the request.
+
+response = requests.get(url)
+
+status = response.status_code
+data = response.json()
+
+print(status)
+print(data)
+
+two_days_ago = data["days"][0]
+yesterday = data["days"][1]
+today = data["days"][2]
+tomorrow = data["days"][3]
+alerts = data["alerts"]
+
+if not alerts:
+    alerts = None
+else:
+    pass # handle 1+ alerts in list
+
+precip_yesterday_in = yesterday["precip"]
+precip_2_days_ago_in = two_days_ago["precip"]
+
+
+# insert data to dataframes
+
+# export dataframe as csv
