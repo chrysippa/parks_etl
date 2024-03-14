b8b58e8d944976987d904ddf55d37ea9b541751e
Cassandra Blair <65232461+chrysippa@users.noreply.github.com>
2024-03-14 12:50:17 -0500

    Add special_days.py

---
+++ special_days.py
@@ -0,0 +1,17 @@
+import pickle
+
+with open('today_data.pickle', 'rb') as today_file:
+    today_data = pickle.load(today_file)
+
+with open('tomorrow_data.pickle', 'rb') as tomorrow_file:
+    tomorrow_data = pickle.load(tomorrow_file)
+
+
+
+
+
+with open('today_data.pickle', 'wb') as today_file:
+    pickle.dump(today_data, today_file)
+
+with open('tomorrow_data.pickle', 'wb') as tomorrow_file:
+    pickle.dump(tomorrow_data, tomorrow_file)
