ca03f6de57f6dcc01189968a756f575cc6c1254b
Cassandra Blair <65232461+chrysippa@users.noreply.github.com>
2024-03-27 16:13:12 -0500

    Add notes for future implementation

--- scrape_dnr_colors.py
+++ scrape_dnr_colors.py
@@ -16,6 +16,9 @@ if today >= this_year_start and today <= this_year_end:
     #with open('tomorrow_data.pickle', 'rb') as tomorrow_file:
     #    tomorrow_data = pickle.load(tomorrow_file)
 
+    #with open('park_metadata.pickle', 'rb') as metadata_file:
+    #    park_metadata = pickle.load(metadata_file)
+
     url = "https://www.dnr.state.mn.us/fall_colors/index.html"
 
     """
@@ -34,10 +37,41 @@ if today >= this_year_start and today <= this_year_end:
     soup = BeautifulSoup(page)
     """
 
+    response = requests.get(url)
+    page = response.text
+
+    soup = BeautifulSoup(page, 'html.parser')
+
+    park_names = {park['name']: park['park_id'] for park in park_metadata}
+
+    # The below is UNTESTED because the page contains JavaScript-rendered content.
+    # Must render the page to get final HTML. Options:
+    # - Requests-HTML: https://requests.readthedocs.io/projects/requests-html/en/latest/
+    # - Playwright: https://github.com/microsoft/playwright-python
+    # - Selenium: see this post https://stackoverflow.com/questions/8049520/how-can-i-scrape-a-page-with-dynamic-content-created-by-javascript-in-python
+
+    # get table
+    table = soup.find(id='summary_table').tbody
+    # for row in table
+    for row in table.children:
+        # get location cell value
+        table_park_name = str(row[1].a.contents)
+        # if location in park_names
+        if table_park_name in park_names.keys():
+            # read that row's color cell value
+            color_cell = row[0]
+            # get the string
+            contents_list = color_cell.stripped_strings
+            # insert to appropriate park dict in today_data list. ensure is string, maybe by unicode(). make sure to trim sides of whitespace
+            park_id = park_names[table_park_name]
+            for park in today_data:
+                if park['park_id'] == park_id:
+                    park['fall_colors'] = contents_list
+                    break
 
 
-    #with open('today_data.pickle', 'wb') as today_file:
-    #    pickle.dump(today_data, today_file)
+        #with open('today_data.pickle', 'wb') as today_file:
+        #    pickle.dump(today_data, today_file)
 
-    #with open('tomorrow_data.pickle', 'wb') as tomorrow_file:
-    #    pickle.dump(tomorrow_data, tomorrow_file)
+        #with open('tomorrow_data.pickle', 'wb') as tomorrow_file:
+        #    pickle.dump(tomorrow_data, tomorrow_file)
