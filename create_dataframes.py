7027791187f4de333e4a24c5bf8b859afcca28cc
Cassandra Blair <65232461+chrysippa@users.noreply.github.com>
2024-03-29 14:17:15 -0500

    Connect to BQ and pull park metadata

--- create_dataframes.py
+++ create_dataframes.py
@@ -1,16 +1,45 @@
+from google.cloud.bigquery_storage import BigQueryReadClient, types
 import pandas as pd
 
-# create connection
-# pull from parks: park_id, name, type, city, accuweather_location
-# pull from special_days: date, park_id, holiday, note (aka all columns)
 
-# Test case: 4 parks
-pulled_data_parks = [
-    ['123', 'Afton State Park', 'state', 'Afton, MN', '24234'],
-    ['456', 'Banning State Park', 'state', 'Nowhere, MN', '32535'],
-    ['789', 'Jay Cooke State Park', 'state', 'Anytown, MN', '65465'],
-    ['987', 'Lake Maria State Park', 'state', 'Village, MN', '86733']
-]
+
+
+# Fetch from database: parks metadata to help construct dataframes
+# Also fetch from special_days to reuse connection
+
+# to authenticate in production, see here: https://cloud.google.com/docs/authentication/provide-credentials-adc#attached-sa
+
+project_id = 'parks-414615'
+
+client = BigQueryReadClient()
+
+table = f'projects/{project_id}/datasets/parks/tables/parks'
+
+requested_session = types.ReadSession()
+requested_session.table = table
+requested_session.data_format = types.DataFormat.AVRO # or can change to Apache Arrow; maybe good for date support
+
+requested_session.read_options.selected_fields = ['park_id', 'name', 'type', 'city', 'site_url'] # columns to pull
+
+parent = f'projects/{project_id}'
+session = client.create_read_session(
+    parent=parent,
+    read_session=requested_session,
+    max_stream_count=1
+)
+reader = client.read_rows(session.streams[0].name)
+
+rows = reader.rows(session)
+
+pulled_data_parks = []
+
+try:
+    for row in rows:
+        pulled_data_parks.append(row) # append 1 dict for row
+except EOFError:
+    pass
+
+# pull from special_days: date, park_id, holiday, note (aka all columns)
 
 
 
@@ -105,38 +134,39 @@ tomorrow_data = pd.DataFrame(data=tomorrow_data_rows)
 
 
 # Create empty dataframe for park metadata - needed for later ETL steps
-
+"""
 metadata_fieldnames = ['park_id', 
                        'name',
                        'type', 
-                       'city', 
-                       'accuweather_location']
+                       'city',
+                       'site_url']
 
 park_metadata_rows = []
 
 # Create 1 row per park. Filled with park_id and value 'None' for other columns.
 for park in pulled_data_parks:
-    park_id = park[0]
+    park_id = park['park_id']
     row = {field: (int(park_id) if field=='park_id' else None) for field in metadata_fieldnames}
     park_metadata_rows.append(row)
 
 # Create dataframe from rows
 park_metadata = pd.DataFrame(data=park_metadata_rows)
+"""
+park_metadata = pd.DataFrame(data=pulled_data_parks)
 
-
-
-# TODO: insert pulled metadata
 
 
 
 # Persist the data
 
-#today_data.to_pickle('today_data.pickle')
+today_data.to_pickle('today_data.pickle')
 
-#tomorrow_data.to_pickle('tomorrow_data.pickle')
+tomorrow_data.to_pickle('tomorrow_data.pickle')
 
-#park_metadata.to_pickle('park_metadata.pickle')
+park_metadata.to_pickle('park_metadata.pickle')
 
+"""
 print(today_data)
 print(tomorrow_data)
 print(park_metadata)
+"""
